347c03fc42aea438c0596bf07482a5295f80b0fc
ajlopez <webmaster@ajlopez.com>
2012-06-06 06:11:38 -0300

    asMessageActor with optional target method name

--- test/asMessageActor.js
+++ test/asMessageActor.js
@@ -10,11 +10,27 @@ exports['Simple Post to Actor'] = function(test) {
     actor.post("foo");
 }
 
+exports['Simple Post to Actor with Target Method Name'] = function(test) {
+    test.expect(2);
+    
+    var obj = new MyObject(test);
+    var actor = simpleactors.asMessageActor(obj, 'method');
+    
+    actor.post("bar");
+}
+
 function MyObject(test) {
     this.process = function(msg)
     {
         test.ok(msg);
         test.equal(msg, "foo");
+        test.done();
+    }
+    
+    this.method = function(msg)
+    {
+        test.ok(msg);
+        test.equal(msg, "bar");
         test.done();
     }
 }
