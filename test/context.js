d045a07d9e79b71d0ab34b7bd8687e85f472bea7
ajlopez <webmaster@ajlopez.com>
2013-02-09 19:48:23 -0300

    Name with relative path in context.actorOf

--- test/context.js
+++ test/context.js
@@ -7,18 +7,41 @@ exports['context in actor'] = function(test) {
     var ref = system.actorOf(myactor);
     
     test.ok(myactor.context);
+    test.ok(myactor.context.self);
+    test.ok(myactor.context.self === ref);
+    
     test.done();
 }
 
 exports['context.actorOf'] = function(test) {
     var system = simpleactors.create('MySystem');
+
     var myactor = new MyActor();
-    var ref = system.actorOf(myactor);
+    var ref = system.actorOf(myactor);    
     var myactor2 = new MyActor();
+
     var actorref = myactor.context.actorOf(myactor2);
     test.ok(actorref);
     test.ok(myactor2);
     test.ok(myactor2.context);
+    test.ok(myactor2.context.self);
+    test.ok(myactor2.context.self === actorref);
+    test.ok(myactor2.context.parent);
+    test.ok(myactor2.context.parent === ref);
+    test.done();
+}
+
+exports['actor created with context has path'] = function(test) {
+    var system = simpleactors.create('MySystem');
+
+    var myactor = new MyActor();
+    var ref = system.actorOf(myactor, 'myactor');
+    var myactor2 = new MyActor();
+
+    var actorref = myactor.context.actorOf(myactor2, 'mychildren');
+    test.ok(actorref);
+    test.ok(actorref.path);
+    test.equal(actorref.path, 'sactors://MySystem/myactor/mychildren');
     test.done();
 }
 
