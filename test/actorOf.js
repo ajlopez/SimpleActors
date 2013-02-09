8d07ba2a5d5ca00a7ccfe022e81b9f79e544df22
ajlopez <webmaster@ajlopez.com>
2013-02-09 15:50:01 -0300

    actorOf first tests

--- test/actorOf.js
+++ test/actorOf.js
@@ -1,12 +1,26 @@
 
 var simpleactors = require('../');
 
-exports['actorOf'] = function(test) {
+exports['actorOf using Function'] = function(test) {
     var system = simpleactors.create('MySystem');
     var actorref = system.actorOf(MyActor);
     
     test.ok(actorref);
     test.equal(typeof system, 'object');
+    test.done();
+}
+
+exports['actorOf using object'] = function(test) {
+    var system = simpleactors.create('MySystem');
+    var myactor = new MyActor();
+    var actorref = system.actorOf(myactor);
+    
+    test.ok(actorref);
+    test.equal(typeof system, 'object');
+    
+    test.ok(myactor.self);
+    test.ok(myactor.self === actorref);
+    
     test.done();
 }
 
