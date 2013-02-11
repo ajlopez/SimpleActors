85f0ba1618fd14c5fe9b3a3d80c5d1bc2c6a9971
ajlopez <webmaster@ajlopez.com>
2013-02-11 17:49:04 -0300

    Router with references, with test

--- test/router.js
+++ test/router.js
@@ -1,9 +1,9 @@
 
 var simpleactors = require('../');
 
-exports['Create Router'] = function(test) {
+exports['Create Router with Actor'] = function(test) {
     function done(msg, sender) {
-        test.ok(msg, 'foo');
+        test.equal(msg, 'foo');
         test.done();
     }
     
@@ -21,6 +21,37 @@ exports['Create Router'] = function(test) {
     test.ok(!actorref.actor);
     
     actorref.tell('foo');
+}
+
+exports['Create Router with Two Actors'] = function(test) {
+    function done1(msg) {
+        if (msg === 'foo')
+            return;
+        test.equal(msg, 'spam');
+        test.done();
+    }
+    
+    function done2(msg) {
+        test.equal(msg, 'bar');
+    }
+    
+    var system = simpleactors.create('MySystem');
+    var myactor1 = new MyActor(done1);
+    var myactor2 = new MyActor(done2);
+    var routerref = system.actorOf(myactor1, 'myactor', { router: true });
+    var actorref2 = system.actorOf(myactor2, 'myactor2');
+    routerref.add(actorref2);
+    
+    test.expect(6);
+    
+    test.ok(myactor1.self === routerref);
+    test.ok(myactor2.self === routerref);
+    test.ok(myactor1.context.self === routerref);
+    test.ok(myactor2.context.self === routerref);
+    
+    routerref.tell('foo');
+    routerref.tell('bar');
+    routerref.tell('spam');
 }
 
 function MyActor(done) {
